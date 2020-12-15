795bf1e4279214068c2f60e3d986296fc4a33e74
Alyona Piatnytskaya <7138748@gmail.com>
2020-12-15 15:26:30 +0200

    Add my finished code

---
+++ src/index.js
@@ -0,0 +1,31 @@
+import './styles.scss';
+import gallery from './menu.json';
+import makeGallery from './templates/gallery-item.hbs';
+const Theme = {
+  LIGHT: 'light-theme',
+  DARK: 'dark-theme',
+};
+
+const galleryDOM = document.querySelector('.js-menu');
+const bodyDOM = document.querySelector('body');
+const changeTheme = document.querySelector('.theme-switch__toggle');
+
+galleryDOM.insertAdjacentHTML('beforeend', makeGallery(gallery));
+if (localStorage.getItem('setting')) {
+  let setting = localStorage.getItem('setting');
+  bodyDOM.classList.add(setting);
+  if (setting === 'dark-theme') {
+    changeTheme.checked = true;
+  }
+}
+changeTheme.addEventListener('change', function () {
+  bodyDOM.className = '';
+  if (this.checked) {
+    bodyDOM.classList.add(Theme.DARK);
+    localStorage.setItem('setting', Theme.DARK);
+  } else {
+    bodyDOM.classList.add(Theme.LIGHT);
+    localStorage.setItem('setting', Theme.LIGHT);
+  }
+});
+
